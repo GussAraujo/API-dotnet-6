ff168cc46999cc54aeb7707b3f0f36eacec9fbe4
GussAraujo <gussaraujo@outlook.com>
2022-02-09 02:06:32 -0300

    Created post method

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 // criando servidor WEB - rodando na porta 7051
 using MiniTodo.Data;
+using MiniTodo.ViewModels;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,18 @@ app.MapGet("/v1/todos", (AppDbContext context) =>
 {
     var todos = context.Todos.ToList();
     return Results.Ok(todos);
+});
+
+app.MapPost("/v1/todos", (AppDbContext context, CreateTodoViewModel model) =>
+{
+    var todo = model.MapTo();
+    if (!model.IsValid)
+        return Results.BadRequest(model.Notifications);
+
+    context.Todos.Add(todo);
+    context.SaveChanges();
+
+    return Results.Created($"/v1/todos/{todo.Id}", todo);
 });
 
 app.Run();
